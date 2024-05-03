5e392a9eb280e660a7f0ac6e629c415a6ff724ae
mlsloynaz <>
2024-05-03 18:18:50 -0400

    polish and cleanup css for layout and bio

--- app/components/bio/bio.tsx
+++ app/components/bio/bio.tsx
@@ -3,24 +3,26 @@ import bgLogo from '/public/images/bg-logo.png'
 
 export default function Bio() {
     return (
-        <div className="flex flex-col w-full h-full  justify-center">
-            <div id="bio" className="flex flex-col h-full bg-gradient-to-b from-blue-800 to-gray-700 text-stone-100 p-4 max-w-96 min-w-72">
-                <h1 className="text-2xl">HI, I am Maria
-                    Sierra Loynaz </h1>
-                <p className="text-l pt-8">Experienced Web developer with a strong focus on JavaScript, specializing in React, Typescript, NodeJs.</p>
+        <div className="flex flex-col w-full h-full max-w-96 min-w-72 md:max-w-full lg:max-w-[1024px]">
+            <div className="flex flex-col md:flex-row md:h-56 lg:h-80">
+                <div id="bio" className="flex flex-col h-full p-4 bg-gradient-to-b from-blue-800 to-gray-700 text-stone-100 max-w-96 min-w-72">
+                    <h1 className="text-2xl py-8">HI, I am Maria
+                        Sierra Loynaz </h1>
+                    <p className="text-l">Experienced Web developer with a strong focus on JavaScript, specializing in React, Typescript, NodeJs.</p>
+                </div>
+                <div id="profile-pic" className="relative h-32 w-full md:h-full lg:h-full">
+                    <Image
+                        src={bgLogo}
+                        alt="Picture of the author"
+                        layout='fill'
+                        objectFit='cover'
+                        placeholder="blur"
+                    />
+                </div>
             </div>
-            <div id="profile-pic" className="relative h-32 w-full flex justify-between">
-                  <Image
-                    src={bgLogo}
-                    alt="Picture of the author"
-                    layout='fill'
-                    objectFit='cover'
-                    placeholder="blur"
-                />
-            </div>
-            <div id="bio-more" className="flex flex-col h-full  text-grey-700 p-4 max-w-96 min-w-72">
+            <div id="bio-more" className="flex flex-col h-full p-4 text-grey-700 ">
                 <p className="text-2xl py-8">Flexible, passionate and quick learner</p>
-                <p className="text-l ">More than 10 years working in Javascript. I am able to navigate seamlessly in the full stack. Known for innovative problem-solving and a keen interest in staying updated with the latest tech trends.</p>
+                <p className="text-l">More than 10 years working in Javascript. I am able to navigate seamlessly in the full stack. Known for innovative problem-solving and a keen interest in staying updated with the latest tech trends.</p>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import Bio from "@/components/bio";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen mt-32 flex-col items-center justify-between px-12 lg:px-28">
-      <div className="flex w-full 
-     bg-stone-200">
+    <main className="flex h-full mt-32 flex-col items-center px-8 lg:px-28 ">
         <Bio />
-      </div>
     </main>
   );
 }
