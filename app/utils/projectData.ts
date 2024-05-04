4dae72c414118db7a5fb1baabc36839f9e07de40
mlsloynaz <>
2024-05-04 11:05:16 -0400

    projects section - added thumbnail component - added projectData - added ProjectData type

---
+++ app/utils/projectData.ts
@@ -0,0 +1,39 @@
+import { ProjectDataType } from "@/definitions/projectDataType";
+
+export const projectData: ProjectDataType[] =[
+    {
+        title:"Barbie",
+        description:"desc",
+        logo: 'barbie-logo.png'
+    },
+    {
+        title:"Royal",
+        description:"desc",
+        logo: 'barbie-logo.png'
+    },
+    {
+        title:"Barbie2",
+        description:"desc",
+        logo:'barbie-logo.png'
+    },
+    {
+        title:"Barbie3",
+        description:"desc",
+        logo:'barbie-logo.png'
+    },
+    {
+        title:"Barbie5",
+        description:"desc",
+        logo:'barbie-logo.png'
+    },
+    {
+        title:"Barbie6",
+        description:"desc",
+        logo:'barbie-logo.png'
+    },
+    {
+        title:"Barbie7",
+        description:"desc",
+        logo:'barbie-logo.png'
+    }
+]
