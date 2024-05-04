03569babb2c146acacb7b02b715616d64e5db79e
mlsloynaz <>
2024-05-04 14:07:24 -0400

    add ids to sections

--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -5,8 +5,8 @@ import { ProjectDataType } from "@/app/definitions/projectDataType";
 export default function Projects() {
     const projects = projectData;
     return (
-        <div className="flex flex-col w-full h-full max-w-96 min-w-72 md:max-w-full lg:max-w-[1024px]">
-            <div id="projects" className="flex flex-col h-full p-4 text-grey-700 ">
+        <div id="project-section" className="flex flex-col w-full h-full max-w-96 min-w-72 md:max-w-full lg:max-w-[1024px]">
+            <div className="flex flex-col h-full p-4 text-grey-700 ">
                 <h2 className="text-2xl pb-8">Experience</h2>
                 <p className="text-l">More than 10 years working in Javascript. I am able to navigate seamlessly in the full stack. Known for innovative problem-solving and a keen interest in staying updated with the latest tech trends.
                 </p>
