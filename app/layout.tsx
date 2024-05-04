e66e59623017ce63fd55fd59c6662706a6a1cb70
mlsloynaz <>
2024-05-04 15:19:09 -0400

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,28 @@ export default function RootLayout({
           </div>
         </header>
         {children}
+        <footer className="flex w-full bg-black h-12 px-8 md:px-28 py-4 text-stone-50">
+          <ul>
+            <li>
+              <a href="https://www.freepik.com/search">Icon by Kiranshastry</a>
+            </li>
+            <li>
+              <a href="https://iconscout.com/icons/angularjs" className="text-underline font-size-sm" target="_blank">Angularjs</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="text-underline font-size-sm">Icon Mafia</a> on <a href="https://iconscout.com" className="text-underline font-size-sm">IconScout</a>
+            </li>
+            <li>
+              <a href="https://iconscout.com/icons/github" className="text-underline font-size-sm" target="_blank">Github</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="text-underline font-size-sm" target="_blank">Icon Mafia</a>
+            </li>
+            <li>
+              <a href="https://iconscout.com/icons/javascript" className="text-underline font-size-sm" target="_blank">Javascript</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="text-underline font-size-sm">Icon Mafia</a> on <a href="https://iconscout.com" className="text-underline font-size-sm">IconScout</a>
+            </li>
+            <li>
+              <a href="https://iconscout.com/icons/node-js" className="text-underline font-size-sm" target="_blank">Node Js</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="text-underline font-size-sm">Icon Mafia</a> on <a href="https://iconscout.com" class="text-underline font-size-sm">IconScout</a>
+            </li>
+            <li>
+              <a href="https://iconscout.com/icons/typescript" className="text-underline font-size-sm" target="_blank">Typescript</a> by <a href="https://iconscout.com/contributors/icon-54" className="text-underline font-size-sm" target="_blank">Icon 54</a>
+            </li>
+          </ul>
+        </footer>
       </body>
     </html>
   );
