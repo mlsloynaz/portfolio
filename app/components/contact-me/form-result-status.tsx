b9a80a689b2937d9b209e4c2736f6cd7eddd9277
mlsloynaz <>
2024-05-10 11:48:24 -0400

    contactme action improvements - save only if sending email failed - fix build errors - cleanup the database

--- app/components/contact-me/form-result-status.tsx
+++ app/components/contact-me/form-result-status.tsx
@@ -21,7 +21,7 @@ function FormResultStatus({ type, message }: ResultStatusProps) {
             case "error":
                 return {
                     icon: (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
                         </svg>
                     ),
@@ -30,7 +30,7 @@ function FormResultStatus({ type, message }: ResultStatusProps) {
             case "warning":
                 return {
                     icon: (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
                         </svg>
                     ),
@@ -41,7 +41,7 @@ function FormResultStatus({ type, message }: ResultStatusProps) {
             default:
                 return {
                     icon: (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                         </svg>
                     ),
@@ -70,7 +70,7 @@ function FormResultStatus({ type, message }: ResultStatusProps) {
                 <p>{message}</p>
             </div>
 
-            {hasCloseButton ? <button aria-label="Close" onClick={handleClose}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            {hasCloseButton ? <button aria-label="Close" onClick={handleClose}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
             </button> : null}
--- app/utils/actions/create-contact-me.ts
+++ app/utils/actions/create-contact-me.ts
@@ -47,26 +47,21 @@ export async function createContactMe(
             result:{message: 'Error validation', type:ResultTypeEnum.ERROR},
         };
     }
+    const emailPayloadToMe = {
+        email: validatedFields.data.email,
+        message: validatedFields.data.message,
+        subject: validatedFields.data.subject,
+        name: validatedFields.data.name,
+    }
+    let errorSendingEmails=false;
 
     try {
-        const emailPayloadToMe = {
-            email: validatedFields.data.email,
-            message: validatedFields.data.message,
-            subject: validatedFields.data.subject,
-            name: validatedFields.data.name,
-        }
-
         const emailPayloadToUser= {
             email: validatedFields.data.email,
             message: "received", // this ios not supposed to be displayed, html created instead
             subject: "Thank you for contacting me!",
             name: validatedFields.data.name
         }
-
-        await prisma.contactMe.create({
-            data : {...emailPayloadToMe}
-        });
-
         await sendEmailToMe(emailPayloadToMe)
 
         await sendEmailToUser(emailPayloadToUser)
@@ -78,10 +73,29 @@ export async function createContactMe(
             resetKey:validatedFields.data.email
         }
     } catch (error) {
-        return {
-            ...prevState,
-            result:{message: 'Submit error', type:ResultTypeEnum.ERROR}
-        };
+        console.log(error)
+        errorSendingEmails=true;
     }
+
+    if(errorSendingEmails){
+        try{
+            await prisma.contactMe.create({
+                data : {...emailPayloadToMe}
+            });
+            return {
+                ...prevState,
+                errors:{},
+                result:{message: 'Info has been saved, you will receive a the next 24h', type:ResultTypeEnum.SUCCESS},
+                resetKey:validatedFields.data.email
+            }
+        }catch(error){
+            console.log(error)
+            return {
+                ...prevState,
+                result:{message: 'Error sending email and saving info, please try again later or use another way', type:ResultTypeEnum.ERROR}
+            };
+        }
+    }
+ 
 }
 
