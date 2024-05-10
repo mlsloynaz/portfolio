ba7feaf36c19770994de74a076ed0b30c3552139
mlsloynaz <>
2024-05-10 15:04:33 -0400

    remove hardcoded in Projects

--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -1,13 +1,13 @@
 import ProjectList from "./projectList";
+import {Projects as t} from  '@/utils/resourceContent';
 
 export default function Projects() {
 
     return (
         <div id="experience-section" className="flex flex-col w-full py-8">
             <div className="flex flex-col h-full text-grey-700 ">
-                <h2 className="text-2xl pb-8">Experience</h2>
-                <p className="text-l">More than 10 years working in Javascript. I am able to navigate seamlessly in the full stack. Known for innovative problem-solving and a keen interest in staying updated with the latest tech trends.
-                </p>
+                <h2 className="text-2xl pb-8">{t.title}</h2>
+                <p className="text-l">{t.description}</p>
             </div>
             <ProjectList />
         </div>
