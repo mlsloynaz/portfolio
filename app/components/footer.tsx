94e65dbac459d65408e9f996faeeab679f7eb570
mlsloynaz <>
2024-05-12 09:19:55 -0400

    Fix phone icon

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,43 +1,43 @@
 export function Footer() {
     return (
-        <footer className="w-full bg-black px-8 md:px-28 py-8 text-stone-50">
+        <footer className="w-full bg-black px-8 md:px-28 py-8 text-stone-50 text-sm">
             <p className="text-2xl pb-8">Credits</p>
             <ul className="list-disc sm:columns-2">
                 <li>
-                    <a href="https://iconscout.com/icons/angularjs" className="underlined underline-offset-2" target="_blank">Angularjs</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="text-sm">IconScout</a>
+                    <a href="https://iconscout.com/icons/angularjs" className="underline underline-offset-2" target="_blank">Angularjs</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="text-sm">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/git" className="underlined underline-offset-2" target="_blank">Git</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/git" className="underline underline-offset-2" target="_blank">Git</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/github" className="underlined underline-offset-2" target="_blank">Github</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/github" className="underline underline-offset-2" target="_blank">Github</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/nodejs" className="underlined underline-offset-2" target="_blank">Nodejs</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2" target="_blank">Icon Mafia</a>
+                    <a href="https://iconscout.com/icons/nodejs" className="underline underline-offset-2" target="_blank">Nodejs</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2" target="_blank">Icon Mafia</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/javascript" className="underlined underline-offset-2" target="_blank">javascript</a> by <a href="https://iconscout.com/contributors/maninderkaur" className="underlined underline-offset-2">maninderkaur</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/javascript" className="underline underline-offset-2" target="_blank">javascript</a> by <a href="https://iconscout.com/contributors/maninderkaur" className="underline underline-offset-2">maninderkaur</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/typescript" className="underlined underline-offset-2" target="_blank">Typescript</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/typescript" className="underline underline-offset-2" target="_blank">Typescript</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/html" className="underlined underline-offset-2" target="_blank">Html</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/html" className="underline underline-offset-2" target="_blank">Html</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/css" className="underlined underline-offset-2" target="_blank">Css</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/css" className="underline underline-offset-2" target="_blank">Css</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/react" className="underlined underline-offset-2" target="_blank">React</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2" target="_blank">Icon Mafia</a>
+                    <a href="https://iconscout.com/icons/react" className="underline underline-offset-2" target="_blank">React</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2" target="_blank">Icon Mafia</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/express" className="underlined underline-offset-2" target="_blank">Express</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underlined underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/express" className="underline underline-offset-2" target="_blank">Express</a> by <a href="https://iconscout.com/contributors/icon-mafia" className="underline underline-offset-2">Icon Mafia</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/chevron-right" className="underlined underline-offset-2" target="_blank">Chevron Right</a> by <a href="https://iconscout.com/contributors/fatkhulkarim" className="underlined underline-offset-2">Fatkhul Karim</a> on <a href="https://iconscout.com" className="underlined underline-offset-2">IconScout</a>
+                    <a href="https://iconscout.com/icons/chevron-right" className="underline underline-offset-2" target="_blank">Chevron Right</a> by <a href="https://iconscout.com/contributors/fatkhulkarim" className="underline underline-offset-2">Fatkhul Karim</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
                 <li>
-                    <a href="https://iconscout.com/icons/phone-call" className="underlined underline-offset-2" target="_blank">Phone Call</a> by <a href="https://iconscout.com/contributors/phosphoricons" className="underlined underline-offset-2" target="_blank">Phosphor Icons</a>
+                <a href="https://iconscout.com/icons/phone" className="underline underline-offset-2" target="_blank">Phone</a> by <a href="https://iconscout.com/contributors/eva-icons" className="underline underline-offset-2">Akveo</a> on <a href="https://iconscout.com" className="underline underline-offset-2">IconScout</a>
                 </li>
             </ul>
         </footer>
