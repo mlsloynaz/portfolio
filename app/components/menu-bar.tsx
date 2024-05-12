939d793498a424d8fdd821679bb67dd23ca4c43c
mlsloynaz <>
2024-05-12 14:32:52 -0400

    close mobile menu after click

--- app/components/menu-bar.tsx
+++ app/components/menu-bar.tsx
@@ -3,18 +3,20 @@
 import Link from "next/link";
 import Image from "next/image";
 import MenuIcon from "./menu-icon";
-import { useState } from "react";
+
+import {Header as t} from  '@/utils/resourceContent';
+import { useMenuContext } from "./projects/MenuContext/menuContext";
 
 export default function MenuBar() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+   const {menuOpen, setMenuOpen} = useMenuContext()
+    
+   const handleMenuMobileClick=()=>{
+        setMenuOpen(false)
+    }
 
     return (
         <>
-            <div className="flex justify-between items-center h-20 px-8 md:px-28 py-4 bg-stone-50">
+            <div className="flex justify-between items-center h-20 px-8 md:px-28 py-4 bg-stone-50 bg-opacity-95">
                 <Link href="/">
                     <Image
                         src="/images/logo.png"
@@ -23,21 +25,21 @@ export default function MenuBar() {
                         height={30}
                         className="inline pr-2"
                     />
-                    <p className="inline">Web Developer</p>
+                    <p className="inline">{t.home}</p>
                 </Link>
                 <div className="hidden gap-4 md:gap-8 md:flex">
                     <MenuItems />
                 </div>
                 <div className="flex justify-self-end items-center">
-                    <Link className="button-blue" href="/contactme">Contact Me</Link>
-                    <div className="ml-4 flex h-8 w-8 items-center justify-center md:hidden" onClick={toggleMenu}>
+                    <Link className="button-blue" href="/contactme">{t.contactMeBtn}</Link>
+                    <div className="ml-4 flex h-8 w-8 items-center justify-center md:hidden">
                         <MenuIcon />
                     </div>
                 </div>
             </div>
             {
-                isOpen ?
-                    <div className="fixed top-32 flex flex-col bg-slate-50 w-full">
+                menuOpen ?
+                    <div className="fixed top-32 flex flex-col bg-slate-50 w-full bg-opacity-95" onClick={handleMenuMobileClick}>
                         <MenuItems />
                     </div> : null
             }
@@ -49,13 +51,13 @@ function MenuItems() {
     return (
         <ul>
             <li className="border-y border-gray-300 p-4 md:border-none md:inline">
-                <Link href="/">About</Link>
+                <Link href="/">{t.about}</Link>
             </li>
             <li className="border-y border-gray-300 p-4 md:border-none md:inline">
-                <Link href="/#experience-section">Experience</Link>
+                <Link href="/#experience-section">{t.experience}</Link>
             </li>
             <li className="border-y border-gray-300 p-4 md:border-none md:inline">
-                <Link href="/#contact-info-section">Contact info</Link>
+                <Link href="/#contact-info-section">{t.contactInfo}</Link>
             </li>
         </ul>
     )
