fceaf3a523f979c1f4f4b4e1c79ec0bb2279496b
mlsloynaz <>
2024-05-15 19:29:31 -0400

    Review Accessibility #35 color contrast

--- app/components/projects/project-thumbnail.tsx
+++ app/components/projects/project-thumbnail.tsx
@@ -13,7 +13,7 @@ export default function ProjectThumbnail({ title, shortDescription, logo }: Proj
         <div className="flex flex-col shrink-0 grow-0 h-48 w-52 bg-[#F6F1E7] p-4 justify-between rounded-lg">
             <div id="projects" className="flex flex-col">
                 <h4 className="text-xl pb-4 text-gray-800 decoration-4">{title}</h4>
-                <p className="text-sm text-gray-400">{shortDescription}</p>
+                <p className="text-sm text-[#9ca3af]">{shortDescription}</p>
             </div>
             <div className="flex justify-between">
                 <Link className="text-md text-grey-800 underline underline-offset-4 decoration-1" href={`project/${title}`}>
