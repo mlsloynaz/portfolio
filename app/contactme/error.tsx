3df3ec65c1cd6663838c124268e74aa724aa4f82
mlsloynaz <>
2024-05-20 18:17:01 -0400

    Add error pages #61

---
+++ app/contactme/error.tsx
@@ -0,0 +1,30 @@
+'use client' // Error components must be Client Components
+
+import { useEffect } from 'react'
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    useEffect(() => {
+        // Log the error to an error reporting service
+    }, [error])
+
+    return (
+        <main className="my-10 flex flex-col justify-center items-center">
+            <h2 className="text-3xl py-10" >Error</h2>
+            <p className="pb-10">Something went wrong!</p>
+            <button className="button-blue"
+                onClick={
+                    // Attempt to recover by trying to re-render the segment
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </main>
+    )
+}
